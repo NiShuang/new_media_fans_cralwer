c66a0f9fb633254f5320df2011a8e48cac22719a
CielNi <562421167@qq.com>
2016-12-08 12:05:10 +0800

    update main.py

--- fans_crawler/main.py
+++ fans_crawler/main.py
@@ -1,15 +1,16 @@
 import datetime
 import json
 
-from fb_crawler import get_by_request as get_fb_fans
+from fb_crawler import get_by_api as get_fb_fans
 from weibo_crawler import get_by_request as get_sina_fans
 from twitter_crawler import get_by_request as get_twitter_fans
-from youtube_crawler import get_by_request as get_youtube_fans
-from youku_crawler import get_by_request as get_youku_fans
-
+from youtube_crawler import get_by_api as get_youtube_fans
+from youku_crawler import get_by_api as get_youku_fans
+from weixin_crawler import get_by_api as get_weixin_fans
+from instagram_crawler import get_by_request as get_instagram_fans
 
 def main():
-    platform = ['facebook', 'weibo', 'twitter', 'youtube', 'youku']
+    platform = ['facebook', 'weibo', 'twitter', 'youtube', 'youku', 'weixin', 'instagram']
     result = []
     for i in platform:
         fans = 0
@@ -24,6 +25,10 @@ def main():
             fans = get_youtube_fans()
         elif i == 'youku':
             fans = get_youku_fans()
+        elif i == 'weixin':
+            fans = get_weixin_fans()
+        elif i == 'instagram':
+            fans = get_instagram_fans()
 
         today = datetime.datetime.now().strftime('%Y-%m-%d')
         temp = {'platform': i, 'fans': fans, 'date': today}
